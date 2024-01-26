c778a410805deaa029dd28c1e690ba720f818f40
rand0m_rob <rob@rand0m.fail>
2024-01-26 18:14:34 +0100

    link instead of copy

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 # shell
-yay -S zsh
-yay -S git
+yay -S zsh   # better bash
+yay -S git   # better svn/cvs
 yay -S kitty # better terminal
 yay -S wget  # better curl
 yay -S less  # better more
 yay -S eza   # better ls
 yay -S dysk  # better df
+yay -S htop  # better top
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 cp config/zshrc ~/.zshrc
 chsh -s /bin/zsh
 
-cp -vr config/kitty ~/.config
+rm -rf ~/.config/kitty && ln -s /opt/install/HyprHypr/config/kitty ~/.config/kitty
+rm ~/.zshrc && ln -s /opt/install/HyprHypr/config/zshrc ~/.zshrc
+rm ~/.vimrc && ln -s /opt/install/HyprHypr/config/vimrc ~/.vimrc
 
 # wm
 yay -S hyprland  # wayland composer
@@ -24,7 +27,10 @@ yay -S webp-pixbuf-loader     # lib to render webp
 yay -S gnome-control-center   # optional gnome settings
 yay -S xdg-desktop-portal-gtk # for desktop sharing needed somehow
 
-cp -vr config/hypr ~/.config
+rm -rf ~/.config/hypr && ln -s /opt/install/HyprHypr/config/hypr ~/.config/hypr
+rm ~/.config/user-dirs.conf && ln -s /opt/install/HyprHypr/config/user-dirs.conf ~/.config/user-dirs.conf
+rm ~/.config/user-dirs.dirs && ln -s /opt/install/HyprHypr/config/user-dirs.dirs ~/.config/user-dirs.dirs
+rm ~/.config/mimeapps.list  && ln -s /opt/install/HyprHypr/config/mimeapps.list ~/.config/mimeapps.list
 
 # waybar
 yay -S waybar                 # info bar
@@ -35,13 +41,15 @@ yay -S pavucontrol            # applet
 yay -S pamixer                # applet
 yay -S mako                   # notifications
 
+rm -rf ~/.config/waybar && ln -s /opt/install/HyprHypr/config/waybar ~/.config/waybar
+rm -rf ~/.config/mako && ln -s /opt/install/HyprHypr/config/mako ~/.config/mako
+
+# Application starter
 yay -R wofi # remove wofi  - obsolete
 yay -S rofi # install rofi - replacement
 
-cp -vr config/waybar ~/.config
-cp -vr config/mako ~/.config
-cp -vr config/rofi ~/.config
-cp -vr local/share/rofi ~/.local/share/rofi
+rm -rf ~/.config/rofi && ln -s /opt/install/HyprHypr/config/rofi ~/.config/rofi
+rm -rf ~/.local/share/rofi && ln -s /opt/install/HyprHypr/local/share/rofi ~/.local/share/rofi
 
 # Applications
 yay -S syncthing
@@ -53,6 +61,10 @@ yay -S discord          # chat for gamers
 yay -S google-chrome    # brwoser :P
 yay -S 1password        # password manager
 yay -S vlc              # media player
+yay -S mc               # Norton Commander clone
+
+rm -rf ~/.config/mc && ln -s /opt/install/HyprHypr/config/mc ~/.config/mc
+rm -rf ~/.config/htop && ln -s /opt/install/HyprHypr/config/htop ~/.config/htop
 
 # proton GE
 wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton8-27/GE-Proton8-27.tar.gz
@@ -71,8 +83,3 @@ yay -S noto-fonts-emoji
 yay -S ttf-jetbrains-mono-nerd
 yay -S ttf-nerd-fonts-symbols-common
 yay -S adobe-source-code-pro-fonts
-
-# other config files
-cp -v config/vimrc ~/.vimrc
-cp -v config/user-dirs.conf ~/.config
-cp -v config/user-dirs.dirs ~/.config
